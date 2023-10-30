acf47fe621965f3090659a6522087f20b494834b
Danny W <dannywang@utexas.edu>
2023-10-29 23:25:26 -0400

    can save events to localStorage; get event from localStorage after page loads

--- docs/script.js
+++ docs/script.js
@@ -31,9 +31,7 @@ function ready(fn) {
 }
 
 function applyClassToTimeBlock() {
-  // const currentHour = dayjs().hour();
-  // console.log(currentHour);
-  const currentHour = 12;
+  const currentHour = dayjs().hour();
 
   for (let i = 9; i < 18; i++) {
     const timeBlock = document.getElementById(`hour-${i}`);
@@ -54,7 +52,43 @@ function displayCurrentDay() {
   currentDayBlock.textContent = currentDayOutput;
 }
 
+function saveToLocalStorage() {
+  const saveButtons = document.getElementsByClassName("saveBtn");
+  for (let i = 0; i < saveButtons.length; i++) {
+    saveButtons.item(i).addEventListener("click", function (e) {
+      console.log(e.target.parentNode.id);
+      const id = e.target.parentNode.id;
+      const childNodes = e.target.parentNode.childNodes;
+      let item;
+      for (let i = 0; i < childNodes.length; i++) {
+        if (childNodes.item(i).nodeName === "TEXTAREA") {
+          item = childNodes.item(i).value;
+        }
+      }
+      console.log(item);
+      localStorage.removeItem(id);
+      localStorage.setItem(id, item);
+    });
+  }
+}
+
+function addSavedEvents() {
+  for (let i = 9; i < 18; i++) {
+    const id = `hour-${i}`;
+    const timeBlock = document.getElementById(id);
+    const item = localStorage.getItem(id);
+    const childNodes = timeBlock.childNodes;
+    for (let i = 0; i < childNodes.length; i++) {
+      if (childNodes.item(i).nodeName === "TEXTAREA") {
+        childNodes.item(i).value = item;
+      }
+    }
+  }
+}
+
 ready(function () {
   applyClassToTimeBlock();
   displayCurrentDay();
+  addSavedEvents();
+  saveToLocalStorage();
 });
